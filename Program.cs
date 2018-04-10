87e39faac59d85fc8f37910fc4129f71097166c4
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-10 04:31:02 +0200

    First commit to continue SoloLearn Course Examples

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace NETCoreSolo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age;
+            Console.WriteLine("Enter a value for age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Value 16 or 42 or 70 will result in non-default case. Entered value was: {0}", age);
+
+            switch (age)
+            {
+                case 16:
+                    Console.WriteLine("Too young");
+                    break;
+                case 42:
+                    Console.WriteLine("Adult");
+                    break;
+                case 70:
+                    Console.WriteLine("Senior");
+                    break;
+                default:
+                    Console.WriteLine("The default case");
+                    break;
+            }
         }
     }
 }
