b8849753622d658d5deb2321c49319a5c2f008f1
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-10 04:26:25 +0200

    Initial dotnet new consolethen dotnet run

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NETCoreSolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
