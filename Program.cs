c4cc270ede88736ee6a37db37d5914e64e81a849
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-21 08:00:15 +0200

    while loop example

--- Program.cs
+++ Program.cs
@@ -6,26 +6,11 @@ namespace NETCoreSolo
     {
         static void Main(string[] args)
         {
-               Console.WriteLine("Hello World!");
-            int age;
-            Console.WriteLine("Enter a value for age:");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Value 16 or 42 or 70 will result in non-default case. Entered value was: {0}", age);
-
-            switch (age)
+            int num = 1;
+            while(num < 6)
             {
-                case 16:
-                    Console.WriteLine("Too young");
-                    break;
-                case 42:
-                    Console.WriteLine("Adult");
-                    break;
-                case 70:
-                    Console.WriteLine("Senior");
-                    break;
-                default:
-                    Console.WriteLine("The default case");
-                    break;
+                Console.WriteLine(num);
+                num++;
             }
         }
     }
