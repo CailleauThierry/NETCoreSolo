2aea06355148fdefa119949936a667a04ab5907d
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-21 08:18:29 +0200

    git credential cache is enabled, this Push should not ask for creds

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace NETCoreSolo
         static void Main(string[] args)
         {
             int num = 0;
-            // runs 5 times pre increments
+            // runs 5 times pre increments, condition evaluated after increment has happended
             while(++num < 6)
                 Console.WriteLine(num);
             num = 0;
