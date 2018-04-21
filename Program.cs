bd140993e9cb9580785e323edcfc2de1fa75f014
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-21 10:05:24 +0200

    for loop with arithmetic operators

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace NETCoreSolo
     {
         static void Main(string[] args)
         {
-            for(int x=10; x< 15;x++)
+            for(int x=10; x<30; x+=3)
+            {
+                Console.WriteLine(format: "Value of x is: {0}", arg0: x);
+            }
+            for(int x=10; x>1; x-=2)
             {
                 Console.WriteLine(format: "Value of x is: {0}", arg0: x);
             }
