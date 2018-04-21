cbfb57f41782db90bbc09952dbd0b7bf1b8c2a47
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-21 09:44:49 +0200

    Foor loop with "format:" + "arg0:" added by C# linter

--- Program.cs
+++ Program.cs
@@ -6,14 +6,10 @@ namespace NETCoreSolo
     {
         static void Main(string[] args)
         {
-            int num = 0;
-            // Condition evaluated after increment has happended, this control loop runs 5 times
-            while(++num < 6)
-                Console.WriteLine(num);
-            num = 0;
-            // Condition evaluated before increment explained why it runs 6 times  
-            while(num++ < 6)
-                Console.WriteLine(num);
+            for(int x=10; x< 15;x++)
+            {
+                Console.WriteLine(format: "Value of x is: {0}", arg0: x);
+            }
         }
     }
 }
