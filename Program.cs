f74d20efa74d0e3c02ed89b4cd488fbfab1c94d0
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-21 09:15:19 +0200

    new change, testing creds

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace NETCoreSolo
         static void Main(string[] args)
         {
             int num = 0;
-            // runs 5 times pre increments, condition evaluated after increment has happended
+            // Condition evaluated after increment has happended, this means this runs 5 times
             while(++num < 6)
                 Console.WriteLine(num);
             num = 0;
