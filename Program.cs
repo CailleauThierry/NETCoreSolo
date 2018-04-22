8f24fa6bbffdecd981def86d13e82d644481914a
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-22 23:47:30 +0200

    Overloading with multiple parameters

--- Program.cs
+++ Program.cs
@@ -8,16 +8,22 @@ namespace NETCoreSolo
         {
             int x = 5;
             double y = 5.5;
+            string txt = "Your variable is";
             Print(x);
             Print(y);
+            Print(txt, 5.6);
         }        
         static void Print(int a)
         {
             Console.WriteLine("Prints int a:"+a);
         }
-            static void Print(double a)
+        static void Print(double a)
         {
             Console.WriteLine("Prints int a:"+a);
+        }
+        static void Print(string label, double a)
+        {
+            Console.WriteLine(label+": "+a);
         }
     }
 }
