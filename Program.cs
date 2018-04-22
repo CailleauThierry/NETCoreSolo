6feaad89a0b67158eec4a431250c1c7e0046ea73
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-22 20:44:19 +0200

    Passing by value in .NET core 2.0

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ namespace NETCoreSolo
         static void Main(string[] args)
         {
             int a = 3;
-            Sqr(ref a);
+            Sqr(a);
             Console.WriteLine("Returned value: {0}", a); //demonstrates passing value by Reference
         }
-        static int Sqr(ref int x)
+        static int Sqr(int x)
         {
             x =x*x;
             return x;
